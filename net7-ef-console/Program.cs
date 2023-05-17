1b353ad361ca8b0cdbe32428bbc5b151b8011835
لطفی نیا ، طراحی وب <E.Lotfi@exir.rayaneh>
2023-05-17 11:24:59 +0330

    fix

---
+++ net7-ef-console/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+var context = new MyDbContext();
+
+var users = context.Users
+    .Where(u => u.IsActive)
+    .OrderBy(u => u.Username);
+
+foreach (var user in users)
+{
+    Console.WriteLine(user.Username);
+}
+
+var newUser = new User() { Username = "johndoe", IsActive = true };
+context.Users.Add(newUser);
+await context.SaveChangesAsync();
+
+public class MyDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=.;Database=Mydatabase;Trusted_Connection=True;Encrypt=False");
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
+
+public class User
+{
+    public int id { get; set; }
+    public string? Username { get; set; }
+    public bool IsActive { get; set; }
+}
